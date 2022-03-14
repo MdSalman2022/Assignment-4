d9fcba6ff5134b63473c2c3d1b6d37c502ec7953
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-03-14 22:00:30 +0600

    Assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Problem - 1 Budget Calculator
+// // //Problem - 1 Budget Calculator
 console.log("----------------------Problem - 1----------------------\n");
 
 function NumberOfProducts(C,W,Cof){
@@ -59,29 +59,47 @@ var products = [
 
 
 // Search your product
-let search = "laptop"
+
+let search = "Laptop"
 
 
 
-//Changing the search string for matching the array
-const nameCapitalized = search.charAt(0).toUpperCase() + search.slice(1).toLowerCase()
-let found = products.find( ({ title }) => title === nameCapitalized );
-//condition to show "product not found" when .find result is undefined
-if(found === undefined){
-    console.log("You serached for: " + nameCapitalized)
-    console.log("\nProduct Not Found");
+for(let i = 0; i < products.length; i++) {
+    
+    if(search === products[i].title){    
+        console.log("You are looking for " + products[i].title + ":");
+        let result = products[i]
+        console.log(result);
+    }
+
+    else if (search !== products[i].title){
+        result = "Product did not matched"
+        // console.log(result);
+    }
 }
-else{
-    console.log("Here is your search result: \n" + nameCapitalized)
-    console.log(found)
-}
+console.log(result);
+
+
+
+// Changing the search string for matching the array
+// const nameCapitalized = search.charAt(0).toUpperCase() + search.slice(1).toLowerCase()
+// let found = products.find( ({ title }) => title === nameCapitalized );
+// condition to show "product not found" when .find result is undefined
+// if(found === undefined){
+//     console.log("You serached for: " + nameCapitalized)
+//     console.log("\nProduct Not Found");
+// }
+// else{
+//     console.log("Here is your search result: \n" + nameCapitalized)
+//     console.log(found)
+// }
 
 
 
 
 
 
-//Problem - 3 Hidden Billionaire Calculator
+// Problem - 3 Hidden Billionaire Calculator
 console.log("\n----------------------Problem - 3----------------------\n");
 
 function SavingYear(year) {
